96264b8b8b9bef626395998a070b97470a1ed122
Alex Charland <ac3charland@gmail.com>
2018-09-28 17:30:28 -0500

    Button adding functionality working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,53 @@
 var key = "uoRDvxXuQcBJD859qYRawyPH26j2wynX";
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=cheezburger";
+var topics = ["happy", "sad", "angry", "jealous"]
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-});
+function displayTopics() {
+    $("#buttons").empty();
+    
+    topics.forEach(function (topic) {
+        var button = $("<button>");
+        button.attr("value", topic);
+        button.addClass("btn");
+        button.addClass("btn-primary");
+        button.addClass("topic");
+        button.text(topic);
+        $("#buttons").append(button);
+    })
+}
+
+$(document).ready(function() {
+    displayTopics();
+
+    $(document).on("click", "#submit", function() {
+        event.preventDefault();
+        var topic = $("#newTopic").val().trim();
+        topics.push(topic);
+        displayTopics();
+    })
+
+    $(document).on("click", ".topic", function() {
+        $("#gif-container").empty();
+        
+        var topic = $(this).attr("value");
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + topic;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            response.data.forEach(element => {
+                var url = element.images.original.url;
+                var title = element.title;
+                
+                var gif = $("<img>");
+                gif.attr("src", url);
+                gif.attr("alt", title);
+
+                $("#gif-container").append(gif);
+            });
+        });
+    })
+})
